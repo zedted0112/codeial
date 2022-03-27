9b580d3f9757548059ddbe875b3e634394f7d828
Nitin Rana <nitinranaofficial456@gmail.com>
2022-03-27 10:17:18 +0530

    authentication for user is done

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const passport=require('passport');
 const userController= require('../controllers/users_controllers');
 
 
-router.get('/profile',userController.profile);
+router.get('/profile',passport.checkAuthentication,userController.profile);
 router.get('/sign-up',userController.signup);
 router.get('/sign-in',userController.signin);
 router.post('/create',userController.create);
